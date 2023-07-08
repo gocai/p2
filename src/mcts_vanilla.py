5f526cc3728b4f0ae1f77b8a67e90c83f01d8567
Gordon Cai <gocai@ucsc.edu>
2023-07-08 14:37:39 -0700

    some comments

--- src/mcts_vanilla.py
+++ src/mcts_vanilla.py
@@ -7,6 +7,9 @@ num_nodes = 1000
 explore_faction = 2.
 
 def traverse_nodes(node, board, state, identity):
+
+    #This is the SELECTION part of MCTS
+
     """ Traverses the tree until the end criterion are met.
 
     Args:
@@ -23,6 +26,9 @@ def traverse_nodes(node, board, state, identity):
 
 
 def expand_leaf(node, board, state):
+
+    #This is the EXPANSION part of MCTS
+
     """ Adds a new leaf to the tree by creating a new child node for the given node.
 
     Args:
@@ -38,6 +44,9 @@ def expand_leaf(node, board, state):
 
 
 def rollout(board, state):
+
+    #This is the SIMULATION part of MCTS
+
     """ Given the state of the game, the rollout plays out the remainder randomly.
 
     Args:
@@ -49,12 +58,18 @@ def rollout(board, state):
 
 
 def backpropagate(node, won):
+
+    #This is the BACKPROPAGATE part of MCTS
+
     """ Navigates the tree from a leaf node to the root, updating the win and visit count of each node along the path.
 
     Args:
         node:   A leaf node.
         won:    An indicator of whether the bot won or lost the game.
-
+    Some Pseudocode from Slides:
+        if node is root return
+        do update states in node
+        backpropagate(node.parent,score)
     """
     pass
 
